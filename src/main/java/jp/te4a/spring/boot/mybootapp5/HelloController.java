e69036f88d99736e7b9ef79402462c7f43f61c28
ogawakaito6 <kaichan6300@gmail.com>
2024-09-05 17:46:23 +0900

    new file:   .vscode/settings.json 	modified:   pom.xml 	deleted:    src/main/java/jp/te4a/spring/boot/mybootapp3/HelloController.java 	new file:   src/main/java/jp/te4a/spring/boot/mybootapp5/HelloController.java 	renamed:    src/main/java/jp/te4a/spring/boot/mybootapp3/Mybootapp3Application.java -> src/main/java/jp/te4a/spring/boot/mybootapp5/Mybootapp5Application.java 	modified:   src/main/resources/application.yml 	new file:   src/main/webapp/WEB-INF/views/index.jsp 	deleted:    src/test/java/jp/te4a/spring/boot/mybootapp3/Mybootapp3ApplicationTests.java 	modified:   target/classes/application.yml 	modified:   target/classes/jp/te4a/spring/boot/mybootapp3/HelloController.class 	new file:   target/classes/jp/te4a/spring/boot/mybootapp5/HelloController.class 	renamed:    target/classes/jp/te4a/spring/boot/mybootapp3/Mybootapp3Application.class -> target/classes/jp/te4a/spring/boot/mybootapp5/Mybootapp5Application.class 	modified:   target/maven-archiver/pom.properties 	modified:   target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	modified:   target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst 	modified:   target/maven-status/maven-compiler-plugin/testCompile/default-testCompile/inputFiles.lst 	new file:   target/mybootapp5-0.0.1-SNAPSHOT.jar 	new file:   target/mybootapp5-0.0.1-SNAPSHOT.jar.original 	modified:   target/surefire-reports/TEST-jp.te4a.spring.boot.mybootapp3.Mybootapp3ApplicationTests.xml 	new file:   target/surefire-reports/TEST-jp.te4a.spring.boot.mybootapp5.Mybootapp5ApplicationTests.xml 	modified:   target/surefire-reports/jp.te4a.spring.boot.mybootapp3.Mybootapp3ApplicationTests.txt 	new file:   target/surefire-reports/jp.te4a.spring.boot.mybootapp5.Mybootapp5ApplicationTests.txt 	deleted:    target/test-classes/jp/te4a/spring/boot/mybootapp3/Mybootapp3ApplicationTests.class

---
+++ src/main/java/jp/te4a/spring/boot/mybootapp5/HelloController.java
@@ -0,0 +1,14 @@
+package jp.te4a.spring.boot.mybootapp5;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController 
+{
+    @RequestMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
